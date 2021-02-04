bec674dd1599e6f0b81d5730af3e9fb22f62c91f
Anmora96 <anmora.2396@gmail.com>
2021-02-04 00:15:46 +0000

    Avance de ejercicios hasta el 7

--- exercises/05-Defining-vs-Calling-a-function/app.js
+++ exercises/05-Defining-vs-Calling-a-function/app.js
@@ -1,5 +1,7 @@
 //Define the function called "multi" that expects 2 parameters:
-
+function multi(val1,val2){
+    return val1 * val2;
+}
 
   //don't edit anything below this line
   var returnValue = multi(7,53812212);
--- exercises/06-Anonymous-functions/app.js
+++ exercises/06-Anonymous-functions/app.js
@@ -4,3 +4,7 @@ var multy = function(a,b){
 //Dont edit anything ABOVE this line
 
 //your code here
+var returnValue = multy(324234,47);
+console.log(returnValue);
+
+module.exports = multy;
